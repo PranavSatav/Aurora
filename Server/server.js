63143bf95c9d276f177b11efc5eedd8e2b2cdb3d
Pranav Satav <madphoenix1311@gmail.com>
2023-01-27 18:37:20 +0530

    updated api keys

--- Server/server.js
+++ Server/server.js
@@ -1,44 +1,48 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import cors from "cors";
-import { Configuration, OpenAIApi } from "openai";
-
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';//cross origin requests
+import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+    });
+const openai = new OpenAIApi(configuration);
 
-const configuration =new Configuration({
-    apiKey:process.env.OPENAI_API_KEY
-})
- 
-const openai= new OpenAIApi(configuration)
-const app=express()
-app.use(cors()) 
-app.use(express.json()) //for sending json data from frontend to backend
+const app = express();
 
+app.use(cors());
 
-app.get("/",async(req,res)=>{
+app.use(express.json());//pass frontend to backend
+
+app.get('/',async(req,res) =>{
     res.status(200).send({
-        message:"Hello World from PranavSatav"
+        message: 'Success',
     })
 })
-app.post('/',async(req,res)=>{
-    try{
-        const prompt=req.body.prompt
-        const response=await openai.createCompletion({
-            model: "text-davinci-003",
+app.post('/',async(req,res) =>{
+    try {
+        const prompt = req.body.prompt;
+
+        const response = await openai.createCompletion({
+            model:"text-davinci-003",
             prompt: `${prompt}`,
-            temperature: 0.5,
-            max_tokens: 3200,
-            top_p: 1,
-            frequency_penalty: 0.5,
-            presence_penalty: 0,
-        })
-        res.status(200).send({
-            bot:response.data.choices[0].text
-        })
-    }catch(error)
-    {
-        console.log(error)
-        res.status(500).send(error || "Something went Wrong")
-    }
+            //amount of risk
+            temperature:0,
+            max_tokens:1000,
+            top_p:1,
+            frequency_penalty:0.6,//chance of it saying the same thing
+            presence_penalty:0,
+            stop:["\"\"\""]
+              
+});
+res.status(200).send({
+    bot: response.data.choices[0].text
+});
+}
+catch(error){
+    console.log(error);
+    res.status(500).send({error});
+}
 })
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+
+app.listen(5000,() =>console.log('Server listening on port https://chatgptbydivesh.onrender.com'));
--- client/script.js
+++ client/script.js
@@ -1,100 +1,119 @@
-import bot from "./assets/bot.gif";
-import user from "./assets/user.gif";
+import bot from './assets/bot.gif';
+import user from './assets/user.gif';
 
-const form = document.querySelector("form");
-const chatContainer = document.getElementById("chat_container")
+const form =document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
+// while loading the chat once the text is put
+//it will show 3 dots one by one
 
-const loader = (element) => {
-    element.textContent = ''
-  loadInterval = setInterval(() => {
-    element.textContent += ".";
-    if (element.textContent==='....') {
-      element.textContent = "";
-    }
-  }, 300);
-};
+function loader(element){
+  element.textContent ='';
+  loadInterval = setInterval(()=>{
+    element.textContent+='.';
+    
+    if(element.textContent === '....')
+      element.textContent='';
+    //it repeats in every 300 milliseconds
+  },300);
+}
 
-const typeText = (element, text) => {
-  let index = 0;
-  let interval = setInterval(() => {
-    if (index < text.length) {
-      element.innerHTML += text.charAt(index);
+//we want it to type word by word
+
+function typeText(element,text) {
+  let index =0;
+  let interval =setInterval(()=>{
+    if(index < text.length){
+      element.innerHTML+= text.charAt(index);
       index++;
-    } else clearInterval(interval);
-  }, 20);
-};
-
-const generateUniqueId = () => {
-  const timeStamp = Date.now();
-  const randomNumber = Math.random();
-  const hexDecimalNumber = randomNumber.toString(16);
-  return `id-${timeStamp}-${hexDecimalNumber}`;
-};
-
-const chatStripe = (isAi, value, UniqueId) => {
-  return `
-        <div class="wrapper ${isAi && "ai"}">
-            <div class="chat">
-                <div class="profile">
-                    <img src="${isAi ? bot : user}" alt="${isAi ? bot : user}"/>
-                </div>
-                <div class="message" id=${UniqueId}>${value}</div>
-            </div>
-        </div>
-
-        `;
-};
-
-const submitHandler = async (e) => {
-  e.preventDefault();
-  const data = new FormData(form);
-
-  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
-  form.reset();
-
-  //Ai chatStripes
-  const UniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, " ", UniqueId);
-  chatContainer.scrollTop = chatContainer.scrollHeight;
-  const messageDiv = document.getElementById(UniqueId)
-    loader(messageDiv)
-
-
-    const response=await fetch("https://aurora-w2ae.onrender.com/",{
-        method:'POST',
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify({
-            prompt:data.get("prompt")
-        })
-    })
-    clearInterval(loadInterval)
-    messageDiv.innerHTML =" "
-    if(response.ok)
-    {
-        const data=await response.json()
-        const parsedData=data.bot.trim()
-        typeText(messageDiv,parsedData)
     }
     else{
-        const err=await response.text();
-        messageDiv.innerHTML="Sorry, Something went wrong. Please try again - Too much requests in 1 hour"
-        console.log(err)
-        
+      clearInterval(interval);
     }
-};
+    //it types text in every 20 milliseconds
+  },20)
+}
 
+// we want a unique id for every timestamp
 
-form.addEventListener("submit",submitHandler)
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalNumber =randomNumber.toString(16);
+  return `id-${timestamp}-${hexadecimalNumber}`;
 
-form.addEventListener('keyup',(e)=>
-{
-    if(e.key==="Enter")
-    {
-        submitHandler(e)
-    }
+}
+function chatStripe (isAi, value, uniqueId) {
+  return (
+    `
+  <div class="wrapper ${isAi && 'ai'}">
+    <div class="chat">
+      <div class="profile">
+      <img
+          src=${isAi ? bot: user}
+          alt="${isAi ? 'bot' : 'user'}"
+      />
+      </div>
+      <div class="message" id=${uniqueId}> ${value}</div>
+    </div>
+  </div>
+  `
+  )
+  }
+
+const handleSubmit = async (e) =>{
+  e.preventDefault();
+  const data = new FormData(form);
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  //reset the form
+  form.reset();
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe (true," ",uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+  loader (messageDiv);
   
+  //this function is used to send the prompt as json , via POST request to the server
+const response = await fetch('https://aurora-w2ae.onrender.com/', {
+  method: 'POST',
+  headers: {
+  'Content-Type': 'application/json'
+},
+body: JSON.stringify({
+  prompt: data.get('prompt')
 })
+})
+
+
+//NOW  remove the ... running in the server 
+clearInterval(loadInterval);
+messageDiv.innerHTML ='';//set the bot to  <NULL>
+
+if(response.ok){
+  const data =await response.json(); //get the response
+  const parseData = data.bot.trim();
+  typeText(messageDiv, parseData);
+}
+else {
+  const err = await response.text();
+  messageDiv.innerHTML = "Oops! Something went wrong Reload!";
+  alert(err);
+}
+
+}
+
+
+
+form. addEventListener('submit', handleSubmit);
+//however we use enter keypress - key 13
+form.addEventListener('keydown',(e)=>{
+
+  if(e.keyCode === 13){
+    handleSubmit(e);
+  }
+});
